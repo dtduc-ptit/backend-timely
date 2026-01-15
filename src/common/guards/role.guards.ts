d3b3792868430f865f56e55aad4ccfcd79e6701b
dtduc-ptit <dtduc.ptit@gmail.com>
2026-01-15 09:59:04 +0700

    feat(Common): update guards common

--- src/common/guards/role.guards.ts
+++ src/common/guards/role.guards.ts
@@ -1,15 +1,47 @@
-import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
-import { Reflector } from '@nestjs/core';
+import {
+  Injectable,
+  CanActivate,
+  ExecutionContext,
+  ForbiddenException,
+  UnauthorizedException,
+} from "@nestjs/common";
+import { Reflector } from "@nestjs/core";
+import { ROLES_KEY } from "./roles.decorator";
+import { Role } from '@prisma/client';
 
 @Injectable()
 export class RolesGuard implements CanActivate {
   constructor(private reflector: Reflector) {}
-  canActivate(context: ExecutionContext): boolean {
-    const roles = this.reflector.get<string[]>('roles', context.getHandler());
-    if (!roles) return true;
 
-    const req = context.switchToHttp().getRequest();
-    const userRole = req.user.role;
-    return roles.includes(userRole);
+  canActivate(context: ExecutionContext): boolean {
+    const requiredRoles = this.reflector.getAllAndOverride<Role[]>(
+      ROLES_KEY,
+      [context.getHandler(), context.getClass()]
+    );
+
+    if (!requiredRoles || requiredRoles.length === 0) {
+      return true;
+    }
+
+    const { user } = context.switchToHttp().getRequest();
+
+    if (!user) {
+      throw new UnauthorizedException("Người dùng chưa được xác thực");
+    }
+    if (!user.role) {
+      throw new ForbiddenException(
+        "Người dùng không có quyền hợp lệ để truy cập"
+      );
+    }
+
+    const hasRole = requiredRoles.includes(user.role);
+
+    if (!hasRole) {
+      throw new ForbiddenException(
+        "Bạn không có quyền truy cập tài nguyên này"
+      );
+    }
+
+    return true;
   }
 }
