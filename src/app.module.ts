8c2b361014ef497bd41bcc7acdf06ae8ea4d80a1
dtduc-ptit <dtduc.ptit@gmail.com>
2026-01-16 13:42:51 +0700

    feat(Reminder): implement api for reminder

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,6 +6,14 @@ import { AuthController } from './modules/auth/auth.controller';
 import { AuthService } from './modules/auth/auth.service';
 import { EventsController } from './modules/events/events.controller';
 import { EventsService } from './modules/events/events.service';
+<<<<<<< Updated upstream
+=======
+import { UsersController } from './modules/users/users.controller';
+import { AdminUsersController } from './modules/users/admin-users.controller';
+import { UsersService } from './modules/users/users.service';
+import { RemindersService } from './modules/reminders/reminders.service';
+import { RemindersController } from './modules/reminders/reminders.controller';
+>>>>>>> Stashed changes
 
 @Module({
   imports: [
@@ -24,8 +32,33 @@ import { EventsService } from './modules/events/events.service';
     }),
   ],
 
+<<<<<<< Updated upstream
   providers: [PrismaService, AuthService, EventsService],
   controllers: [AuthController, EventsController, EventsController],
   exports: [PrismaService, AuthService, EventsService],
+=======
+  providers: [
+    PrismaService,
+    AuthService,
+    EventsService,
+    UsersService,
+    RemindersService,
+  ],
+  controllers: [
+    AuthController,
+    EventsController,
+    EventsController,
+    UsersController,
+    AdminUsersController,
+    RemindersController,
+  ],
+  exports: [
+    PrismaService,
+    AuthService,
+    EventsService,
+    UsersService,
+    RemindersService,
+  ],
+>>>>>>> Stashed changes
 })
 export class AppModule {}
---
+++ src/modules/reminders/reminders.controller.ts
@@ -0,0 +1,26 @@
+import { Controller, Post, Get, Param, Body } from '@nestjs/common';
+import { RemindersService } from './reminders.service';
+import { CreateReminderDto } from './dto/create-reminder.dto';
+import { CurrentUser } from 'src/common/decorators/auth.decorators';
+
+@Controller('events/:eventId/reminders')
+export class RemindersController {
+  constructor(private readonly remindersService: RemindersService) {}
+
+  @Post()
+  create(
+    @Param('eventId') eventId: string,
+    @CurrentUser('id') userId: string,
+    @Body() dto: CreateReminderDto,
+  ) {
+    return this.remindersService.create(eventId, userId, dto);
+  }
+
+  @Get()
+  getAll(
+    @Param('eventId') eventId: string,
+    @CurrentUser('id') userId: string,
+  ) {
+    return this.remindersService.getByEvent(eventId, userId);
+  }
+}
