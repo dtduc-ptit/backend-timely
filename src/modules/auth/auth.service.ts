a15c5ff2738edcc1490c1d1e5e4cc1a4b35fc866
dtduc-ptit <dtduc.ptit@gmail.com>
2026-01-21 13:16:37 +0700

    feat(Auth): update logic for login and register

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -6,6 +6,7 @@ import { LoginDto } from './dto/login.dto';
 import bcrypt from 'bcrypt';
 import { UserResponseDto } from './dto/authResponse.dto';
 import { plainToInstance } from 'class-transformer';
+import { Role, User } from '@prisma/client';
 
 @Injectable()
 export class AuthService {
@@ -14,9 +15,7 @@ export class AuthService {
     private readonly jwtService: JwtService,
   ) {}
 
-  async register(
-    dto: RegisterDto,
-  ): Promise<{ user: UserResponseDto; token: string }> {
+  async register(dto: RegisterDto): Promise<{ user: UserResponseDto }> {
     const hashed = await bcrypt.hash(dto.password, 10);
 
     const user = await this.prisma.user.create({
@@ -28,45 +27,62 @@ export class AuthService {
       },
     });
 
-    const token = this.signToken(user.id);
-
     return {
       user: plainToInstance(UserResponseDto, user, {
         excludeExtraneousValues: true,
       }),
-      token,
     };
   }
 
-  async login(
-    dto: LoginDto,
-  ): Promise<{ user: UserResponseDto; token: string }> {
-    const user = await this.prisma.user.findUnique({
-      where: { email: dto.email },
-    });
-
-    if (!user) {
-      throw new UnauthorizedException('User not found');
-    }
+  async login(dto: LoginDto): Promise<{
+    user: UserResponseDto;
+    access_token: string;
+    refresh_token: string;
+  }> {
+    const user = await this.validateUserCredentials(dto);
 
     const match = await bcrypt.compare(dto.password, user.password);
     if (!match) {
       throw new UnauthorizedException('Invalid credentials');
     }
 
-    const token = this.signToken(user.id);
+    const access_token = await this.generateToken(user.id, user.role, 900);
+    const refresh_token = await this.generateToken(user.id, user.role, 604800);
 
     return {
       user: plainToInstance(UserResponseDto, user, {
         excludeExtraneousValues: true,
       }),
-      token,
+      access_token,
+      refresh_token,
     };
   }
 
-  signToken(userId: number): string {
-    return this.jwtService.sign({
-      sub: userId,
+  async validateUserCredentials(loginDto: LoginDto): Promise<User> {
+    const user = await this.prisma.user.findUnique({
+      where: { email: loginDto.email },
     });
+    if (!user) {
+      throw new UnauthorizedException('Tài khoản hoặc mật khẩu không đúng');
+    }
+    const isPasswordValid = await bcrypt.compare(
+      loginDto.password,
+      user.password,
+    );
+    if (!isPasswordValid) {
+      throw new UnauthorizedException('Tài khoản hoặc mật khẩu không đúng');
+    }
+    return user;
+  }
+
+  async generateToken(
+    userId: number,
+    role: Role,
+    accessTime: number,
+  ): Promise<string> {
+    return this.jwtService.sign(
+      { sub: userId, role: role },
+      { expiresIn: `${accessTime}s` },
+    );
   }
 }
