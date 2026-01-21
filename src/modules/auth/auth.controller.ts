9f156d8732d444e570e483b7dcdf99acb9498322
dtduc-ptit <dtduc.ptit@gmail.com>
2026-01-21 09:58:23 +0700

    fix(db): update schema prisma

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, HttpCode, Post } from '@nestjs/common';
 import { RegisterDto } from './dto/register.dto';
 import { LoginDto } from './dto/login.dto';
 import { AuthService } from './auth.service';
@@ -12,6 +12,7 @@ export class AuthController {
     return this.authService.register(dto);
   }
 
+  @HttpCode(200)
   @Post('login')
   async login(@Body() dto: LoginDto) {
     return this.authService.login(dto);
